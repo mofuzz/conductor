e190e5b950e3d2c1174d0764f85bcc7dab6b3789
Morgan Packard <morgan@morganpackard.com>
2014-08-13 11:50:17 -0400

    changed server port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var app = require('http').createServer(handler).listen(80),
+var app = require('http').createServer(handler).listen(8080),
     fs = require('fs'),
     io = require('socket.io').listen(app);
 
